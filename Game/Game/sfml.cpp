902ed86c90c5776366dbc3a597ba2b6d4842668c
IVBadalov21 <IVBadalov21@codingburgas.bg>
2023-11-08 09:10:48 +0200

    Make the menu

---
+++ Game/Game/sfml.cpp
@@ -0,0 +1,17 @@
+#include <SFML/Graphics.hpp>
+
+#include <MainMenu.h>
+
+using namespace sf;
+
+void main() {
+
+	RenderWindow Menu(VideoMode(1080, 720), "Main Menu", Style:Default);
+
+	MainMenu mainMenu(MENU.getSize().x, MENU.getsize().y);
+
+	while (MENU.isOpen()) {
+		EVent 
+	}
+
+}
